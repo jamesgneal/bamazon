fddd64db85b6d16ef4007fcfeffefb11a12db0bc
Graham Neal <nealjg@gmail.com>
2018-04-18 13:44:41 -0400

    functionality minimums met

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,15 +18,17 @@ connection.connect(function (err) {
     start();
 });
 var quantArr = [];
+
 function start() {
     quantArr = [];
     var query = "SELECT * FROM products";
     connection.query(query, function (err, res) {
         console.log(`\n=================================\n=  Items Available to Purchase  =\n=================================\n`);
         for (var i = 0; i < res.length; i++) {
-            console.log(`${res[i].item_id} ${res[i].product_name} ${res[i].department_name} $${res[i].price} ${res[i].stock_quantity}`);
-            quantArr.push(res[i].stock_quantity);
+            console.log(`${res[i].item_id}: ${res[i].product_name}, ${res[i].stock_quantity} available, $${res[i].price}`);
+            quantArr.push({product: res[i].product_name, quantity: res[i].stock_quantity, price: res[i].price});
         }
+        console.log(`\n`);
         whichItem();
     });
 }
@@ -34,22 +36,39 @@ function start() {
 function whichItem() {
     inquirer
         .prompt([{
-            name: "which_item",
-            type: "input",
-            message: "Enter the ID number of the item you would like to purchase: "
-        },
-        {
-            name: "how_many",
-            type: "input",
-            message: "Enter the purchase quantity: "
-        }])
+                name: "which_item",
+                type: "input",
+                message: "Enter the ID number of the item you would like to purchase:"
+            },
+            {
+                name: "how_many",
+                type: "input",
+                message: "Enter the purchase quantity:"
+            }
+        ])
         .then(function (user) {
             var adjID = user.which_item - 1;
             // console.log(adjID);
             // console.log(quantArr[adjID]);
-            if (user.how_many > quantArr[adjID]) {
-                console.log(`I'm sorry, only ${quantArr[adjID]} available.`)
+            if (user.how_many > quantArr[adjID].quantity) {
+                console.log(`\nI'm sorry, only ${quantArr[adjID].quantity} available.`);
                 start();
+            } else {
+                var totalPrice = user.how_many * quantArr[adjID].price;
+                console.log(`\nYour order has been placed!\nItem: ${quantArr[adjID].product}\nQuantity Purchased: ${user.how_many}\nTotal Price: $${totalPrice}\n`);
+                var newQuant = quantArr[adjID].quantity - user.how_many;
+                connection.query("UPDATE products SET ? WHERE ?",
+                [
+                  {
+                    stock_quantity: newQuant
+                  },
+                  {
+                    item_id: user.which_item
+                  }
+                ], function (err, res) {
+                    console.log(`\n***  Thank You For Your Purchase!  ***\n`)
+                });
+                connection.end();
             }
         });
 }
